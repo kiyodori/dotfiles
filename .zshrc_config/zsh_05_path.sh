964b944ac17f738f9449ff437e45f0ad8ea7e8b4
kiyodori <kiyodori@yahoo.co.jp>
2017-01-04 15:22:15 +0900

    coreutils用設定

--- .zshrc_config/zsh_05_path.sh
+++ .zshrc_config/zsh_05_path.sh
@@ -1,6 +1,9 @@
-# postgres用設定
+# postgres用
 export PGDATA=/usr/local/var/postgres
 export ARCHFLAGS="-arch x86_64"
 
 # yarn用
 export PATH="$PATH:`yarn global bin`"
+
+# coreutils用
+export PATH=$(brew --prefix coreutils)/libexec/gnubin:$PATH
