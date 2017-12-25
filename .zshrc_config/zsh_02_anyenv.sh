c3c0b32ff996fd52c1275e320016809965b514d2
kiyodori <kiyodori@yahoo.co.jp>
2017-12-25 10:20:45 +0900

    tmuxのパスを通す

--- .zshrc_config/zsh_02_anyenv.sh
+++ .zshrc_config/zsh_02_anyenv.sh
@@ -1,4 +1,8 @@
 if [ -d $HOME/.anyenv ] ; then
-        export PATH="$HOME/.anyenv/bin:$PATH"
-        eval "$(anyenv init -)"
+    export PATH="$HOME/.anyenv/bin:$PATH"
+    eval "$(anyenv init -)"
+    for D in `find $HOME/.anyenv/envs -type d -d 1`
+    do
+        export PATH="$D/shims:$PATH"
+    done
 fi
