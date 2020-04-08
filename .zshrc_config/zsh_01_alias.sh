38b20860d6a1fba439b00b9c4c71640d5b1feda6
kiyodori <kiyodori@yahoo.co.jp>
2020-04-09 06:58:04 +0900

    zshの補完と表示を変更

--- .zshrc_config/zsh_01_alias.sh
+++ .zshrc_config/zsh_01_alias.sh
@@ -2,6 +2,8 @@ alias ll='ls -l'
 alias la='ls -a'
 alias vi='vim'
 alias grep='grep --color'
+alias cdcode='cd ~/Documents/code'
+alias cdde='cd ~/Documents/code/dokugaku-engineer'
 
 # Global aliases
 alias -g L='| less'
